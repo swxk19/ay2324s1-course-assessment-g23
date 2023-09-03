dbf6d34bab3896734a5c05e3d3486ae60e3a25ab
EvitanRelta <shauntanzongzhi@gmail.com>
2023-09-03 17:43:02 +0800

    Draft TS-docs for functions/interface

--- frontend/src/services/questionBank.ts
+++ frontend/src/services/questionBank.ts
@@ -1,6 +1,9 @@
 /** `localStorage` key for storing questions. */
 const QUESTIONS_STORAGE_KEY = 'questions'
 
+/**
+ * Represents a question in the question bank.
+ */
 export interface Question {
     id: number
     title: string
@@ -9,6 +12,12 @@ export interface Question {
     complexity: 'Easy' | 'Medium' | 'Hard'
 }
 
+/**
+ * Gets the next available ID for storing a new question.
+ *
+ * @private
+ * @returns {Promise<number>} The next available ID.
+ */
 const _getNextId = async (): Promise<number> => {
     const questions: Question[] = await getQuestions()
     if (questions.length === 0) return 1
@@ -17,7 +26,12 @@ const _getNextId = async (): Promise<number> => {
     return maxId + 1
 }
 
-// Create
+/**
+ * Stores a new question into the localStorage.
+ *
+ * @param {Omit<Question, 'id'>} question - The question object without the ID.
+ * @returns {Promise<void>}
+ */
 export const storeQuestion = async (
     question: Omit<Question, 'id'>
 ): Promise<void> => {
@@ -29,12 +43,21 @@ export const storeQuestion = async (
     localStorage.setItem(QUESTIONS_STORAGE_KEY, JSON.stringify(questions))
 }
 
-// Read
+/**
+ * Retrieves all questions from the localStorage.
+ *
+ * @returns {Promise<Question[]>} An array of questions.
+ */
 export const getQuestions = async (): Promise<Question[]> => {
     return JSON.parse(localStorage.getItem(QUESTIONS_STORAGE_KEY) || '[]')
 }
 
-// Update
+/**
+ * Updates an existing question in the localStorage.
+ *
+ * @param {Question} updatedQuestion - The updated question object.
+ * @returns {Promise<void>}
+ */
 export const updateQuestion = async (
     updatedQuestion: Question
 ): Promise<void> => {
@@ -47,7 +70,12 @@ export const updateQuestion = async (
     }
 }
 
-// Delete
+/**
+ * Deletes a question by its ID from the localStorage.
+ *
+ * @param {number} id - The ID of the question to be deleted.
+ * @returns {Promise<void>}
+ */
 export const deleteQuestion = async (id: number): Promise<void> => {
     const questions: Question[] = await getQuestions()
     const newQuestions = questions.filter((q) => q.id !== id)
