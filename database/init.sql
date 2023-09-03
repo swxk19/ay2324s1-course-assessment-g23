0524a82a95cfae7942f7e5d46da4eba6750b125b
Lava-Iris <lavanya.m1804@gmail.com>
2023-09-04 06:36:22 +0800

    remove bug to allow creation of database

--- database/init.sql
+++ database/init.sql
@@ -2,5 +2,5 @@ CREATE TABLE users(
     user_id VARCHAR(255) PRIMARY KEY,
     username VARCHAR(255) NOT NULL,
     password VARCHAR(255) NOT NULL,
-    email VARCHAR(255) UNIQUE NOT NULL,
+    email VARCHAR(255) UNIQUE NOT NULL
 );
