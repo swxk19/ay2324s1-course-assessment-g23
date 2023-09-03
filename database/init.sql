dc113907962f59d99d51b78d5310e9d218976029
swxk19 <s.wxk19@gmail.com>
2023-09-04 04:10:25 +0800

    Fix syntax error in init.sql

--- database/init.sql
+++ database/init.sql
@@ -1,6 +1,6 @@
-CREATE TABLE users(
+CREATE TABLE users (
     user_id VARCHAR(255) PRIMARY KEY,
     username VARCHAR(255) NOT NULL,
     password VARCHAR(255) NOT NULL,
-    email VARCHAR(255) UNIQUE NOT NULL,
+    email VARCHAR(255) UNIQUE NOT NULL
 );
