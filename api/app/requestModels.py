b0e414abee546562a02db3353419966bbcea0c5f
EvitanRelta <shauntanzongzhi@gmail.com>
2023-09-07 20:58:04 +0800

    Fix get/delete backend API
    Now the question id is in the URL instead of the request body.

--- api/app/requestModels.py
+++ api/app/requestModels.py
@@ -24,15 +24,9 @@ class CreateQuestion(BaseModel):
     category: str
     complexity: str
 
-class GetQuestion(BaseModel):
-    question_id: str
-
 class UpdateQuestionInfo(BaseModel):
     question_id: str
     title: str
     description: str
     category: str
     complexity: str
-
-class DeleteQuestion(BaseModel):
-    question_id: str
