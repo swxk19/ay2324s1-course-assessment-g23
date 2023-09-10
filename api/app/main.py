bed8258928ff96d14410550d30426466670f0f43
swxk19 <s.wxk19@gmail.com>
2023-09-10 20:02:33 +0800

    Fix calling functions from incorrect paths

--- api/app/main.py
+++ api/app/main.py
@@ -4,6 +4,7 @@ from fastapi.middleware.cors import CORSMiddleware
 
 import requestModels as rm
 import database as db
+from database_functions import user_functions as uf
 
 # create app
 app = FastAPI()
@@ -16,25 +17,23 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-@app.post("/users", status_code=200)
+@app.post("/users")
 async def create_user(r: rm.CreateUser):
     user_id = str(uuid.uuid4())
-    if (db.create_user(user_id, r.username, r.email, r.password)):
-        return {"user_id": user_id}
-    else:
-        return {"message": "Invalid creation"} # placeholder message. Better to specify invalid fields
+    return uf.create_user(user_id, r.username, r.email, r.password)
+
 
 @app.get("/users/{user_id}")
 async def get_user(user_id: str):
-    return db.get_user(user_id)
+    return uf.get_user(user_id)
 
 @app.delete("/users/{user_id}", status_code=200)
 async def delete_user(user_id: str):
-    return db.delete_user(user_id)
+    return uf.delete_user(user_id)
 
 @app.put("/users", status_code=200)
 async def update_user_info(r: rm.UpdateUserInfo):
-    if (db.update_user_info(r.user_id, r.username, r.password, r.email)):
+    if (uf.update_user_info(r.user_id, r.username, r.password, r.email)):
         return {"message": "Updated Successfully"}
     else:
         return {"message": "Invalid update"} # placeholder message. Better to specify why invalid
