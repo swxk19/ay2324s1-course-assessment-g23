5f4981d0a6dcbfd7ed6b4fd2cbab0c0925d6f421
swxk19 <s.wxk19@gmail.com>
2023-09-22 05:01:53 +0800

    Move session checking to API Gateway

--- backend_services/questions/api/main.py
+++ backend_services/questions/api/main.py
@@ -10,8 +10,6 @@ from .controllers import questions_controller as qc
 
 API_GATEWAY_URL = "http://localhost:8000"
 
-USER_PERMISSION = 0
-MAINTAINER_PERMISSION = 1
 # create app
 app = FastAPI()
 
@@ -23,53 +21,19 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-async def check_session(request: Request, permssion_required):
-    def map_role_permission(role):
-        if role == "user":
-            return USER_PERMISSION
-        elif role == "maintainer":
-            return MAINTAINER_PERMISSION
-        return -1
-
-    session_id = request.cookies.get('session_id')
-
-    url = f'{API_GATEWAY_URL}/sessions'
-    headers = { session_id: session_id }
-    response = requests.get(url, headers=headers)
-
-
-    async with httpx.AsyncClient() as client:
-        response = await client.get(url, headers=headers)
-
-        if response.status_code != 200:
-            raise HTTPException(status_code=response.status_code, detail=response.text)
-
-        permission_level = map_role_permission(response.json()["role"])
-
-        if permission_level < permssion_required:
-            raise HTTPException(status_code=401, detail="Unauthorized access")
-
 @app.post("/questions", status_code=200)
 async def create_question(r: rm.CreateQuestion, request: Request):
-    await check_session(request, MAINTAINER_PERMISSION)
-
     question_id = str(uuid.uuid4())
     return qc.create_question(question_id, r.title, r.description, r.category, r.complexity, r.session_id)
 
 @app.get("/questions/{question_id}", status_code=200)
-async def get_question(question_id: str, r: rm.GetQuestion, request: Request):
-    await check_session(request, USER_PERMISSION)
-
+async def get_question(question_id: str, r: rm.GetQuestion):
     return qc.get_question(question_id, r.session_id)
 
 @app.put("/questions", status_code=200)
-async def update_question_info(r: rm.UpdateQuestionInfo, request: Request):
-    await check_session(request, MAINTAINER_PERMISSION)
-
+async def update_question_info(r: rm.UpdateQuestionInfo):
     return qc.update_question_info(r.question_id, r.title, r.description, r.category, r.complexity, r.session_id)
 
 @app.delete("/questions/{question_id}", status_code=200)
-async def delete_question(question_id: str, r: rm.DeleteQuestion, request: Request):
-    await check_session(request, MAINTAINER_PERMISSION)
-
+async def delete_question(question_id: str, r: rm.DeleteQuestion):
     return qc.delete_question(question_id, r.session_id)
