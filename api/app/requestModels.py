1a25f6e0b491f29661c958cc74af58685d6798e3
EvitanRelta <shauntanzongzhi@gmail.com>
2023-09-22 14:58:26 +0800

    Move session ID to cookies
    Temp fix for #60

--- api/app/requestModels.py
+++ api/app/requestModels.py
@@ -6,44 +6,25 @@ class CreateUser(BaseModel):
     password: str
     email: str
 
-class GetUser(BaseModel):
-    session_id: str
-
 class UpdateUserInfo(BaseModel):
     user_id: str
     username: str
     password: str
     email: str
     role: str
-    session_id: str
-
-# class UpdateUserRole(BaseModel):
-#     role: str
-#     session_id: str
-
-class DeleteUser(BaseModel):
-    session_id: str
 
 class CreateQuestion(BaseModel):
     title: str
     description: str
     category: str
     complexity: str
-    session_id: str
     
-class GetQuestion(BaseModel):
-    session_id: str
-
 class UpdateQuestionInfo(BaseModel):
     question_id: str
     title: str
     description: str
     category: str
     complexity: str
-    session_id: str
-
-class DeleteQuestion(BaseModel):
-    session_id: str
 
 class UserLogin(BaseModel):
     username: str
