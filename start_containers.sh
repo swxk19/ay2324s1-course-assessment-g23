1e61573f8afc374d9c104cb7498fca97ad124362
EvitanRelta <shauntanzongzhi@gmail.com>
2023-10-04 19:22:00 +0800

    Update 'start_container.sh' to create volumes if they don't exist

--- start_containers.sh
+++ start_containers.sh
@@ -1,3 +1,13 @@
+#!/bin/bash
+
+# Check if volumes already exist, if not create them
+if [ "$(docker volume ls | grep users-data)" == "" ]; then
+    docker volume create users-data
+fi
+if [ "$(docker volume ls | grep questions-data)" == "" ]; then
+    docker volume create questions-data
+fi
+
 # Bring down the services and remove all images, before building/starting.
 docker-compose down --rmi all \
     && docker-compose --env-file .env up --build "$@"
