5772f30d6728296615a3760727c140874e3dcc09
swxk19 <s.wxk19@gmail.com>
2023-10-20 23:47:44 +0800

    Setup collaboration service

---
+++ backend_services/collaboration_service/api/app/main.py
@@ -0,0 +1,14 @@
+from fastapi import FastAPI, Request
+from fastapi.middleware.cors import CORSMiddleware
+
+# create app
+app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+    expose_headers=["*"],
+)
