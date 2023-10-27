b8449d0ed092a1aaae3f3da1c7e26cc94b7a388d
EvitanRelta <shauntanzongzhi@gmail.com>
2023-10-27 15:55:51 +0800

    Replace returning 'ServiceError' with raising 'HTTPException' in 'sessions_controller'

--- backend_services/users_service/api/app/controllers/sessions_controller.py
+++ backend_services/users_service/api/app/controllers/sessions_controller.py
@@ -1,11 +1,11 @@
 import hashlib
-from api_models.error import ServiceError
 from api_models.users import GetSessionResponse, UserLoginResponse, UserLogoutResponse
+from fastapi import HTTPException
 from user_database import USER_DATABASE as db
 from utils import users_util, sessions_util
 from datetime import datetime
 
-def user_login(username: str, password: str) -> UserLoginResponse | ServiceError:
+def user_login(username: str, password: str) -> UserLoginResponse:
     hashed_password = hashlib.md5(password.encode()).hexdigest()
 
     login_result = sessions_util.is_valid_login(username, hashed_password) # returns False if invalid login
@@ -16,8 +16,8 @@ def user_login(username: str, password: str) -> UserLoginResponse | ServiceError
         return UserLoginResponse(session_id=session_id, message=f'User {username} successfully logged in')
     else:
         if users_util.username_exists(username):
-            return ServiceError(status_code=401, message='Invalid password')
-        return ServiceError(status_code=401, message='Account does not exist')
+            raise HTTPException(status_code=401, detail='Invalid password')
+        raise HTTPException(status_code=401, detail='Account does not exist')
 
 def get_all_sessions() -> list[GetSessionResponse]:
     FIELD_NAMES = ['session_id', 'user_id', 'role', 'creation_time', 'expiration_time']
@@ -26,14 +26,14 @@ def get_all_sessions() -> list[GetSessionResponse]:
     sessions = [dict(zip(FIELD_NAMES, row)) for row in rows]
     return [GetSessionResponse(**x) for x in sessions]
 
-def get_session(session_id: str) -> GetSessionResponse | ServiceError:
+def get_session(session_id: str) -> GetSessionResponse:
     FIELD_NAMES = ['session_id', 'user_id', 'role', 'creation_time', 'expiration_time']
 
     result = db.execute_sql_read_fetchone('SELECT * FROM sessions WHERE session_id = %s',
                                  params=(session_id,))
 
     if result is None:
-        return ServiceError(status_code=401, message='Unauthorized session')
+        raise HTTPException(status_code=401, detail='Unauthorized session')
 
     expiration_time = result[4]
     assert isinstance(expiration_time, datetime), \
@@ -44,12 +44,12 @@ def get_session(session_id: str) -> GetSessionResponse | ServiceError:
         converted = [x.isoformat() if isinstance(x, datetime) else x for x in result]
         return GetSessionResponse(**dict(zip(FIELD_NAMES, converted)))
 
-    return ServiceError(status_code=401, message='Unauthorized session')
+    raise HTTPException(status_code=401, detail='Unauthorized session')
 
-def user_logout(session_id: str) -> UserLogoutResponse | ServiceError:
+def user_logout(session_id: str) -> UserLogoutResponse:
     try:
         sessions_util.delete_session(session_id)
         return UserLogoutResponse(message=f'Session {session_id} successfully deleted')
     except Exception as e:
-        return ServiceError(status_code=500, message=f'Unable to logout user: {e}')
+        raise HTTPException(status_code=500, detail=f'Unable to logout user: {e}')
 
