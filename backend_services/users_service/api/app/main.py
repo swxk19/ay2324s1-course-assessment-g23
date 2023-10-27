2e6155414b4071ec7a958844482dbc49ee43e90f
EvitanRelta <shauntanzongzhi@gmail.com>
2023-10-27 15:49:10 +0800

    Remove 'status_code=200' & 'response_model=None' from users-service APIs

--- backend_services/users_service/api/app/main.py
+++ backend_services/users_service/api/app/main.py
@@ -17,47 +17,47 @@ app.add_middleware(
     expose_headers=["*"]
 )
 
-@app.post("/users", status_code=200, response_model=None)
+@app.post("/users")
 async def create_user(r: CreateUserRequest) -> CreateUserResponse:
     user_id = str(uuid.uuid4())
     return uc.create_user(user_id, r.username, r.email, r.password)
 
-@app.get("/users/{user_id}", status_code=200, response_model=None)
+@app.get("/users/{user_id}")
 async def get_user(user_id: str) -> GetUserResponse:
     return uc.get_user(user_id)
 
-@app.get("/users_all", status_code=200, response_model=None)
+@app.get("/users_all")
 async def get_all_users() -> list[GetUserResponse]:
     return uc.get_all_users()
 
-@app.delete("/users_all", status_code=200, response_model=None)
+@app.delete("/users_all")
 async def delete_all_users() -> DeleteUserResponse:
     return uc.delete_all_users()
 
-@app.delete("/users/{user_id}", status_code=200, response_model=None)
+@app.delete("/users/{user_id}")
 async def delete_user(user_id: str) -> DeleteUserResponse:
     return uc.delete_user(user_id)
 
-@app.put("/users/{user_id}", status_code=200, response_model=None)
+@app.put("/users/{user_id}")
 async def update_user_info(user_id: str, r: UpdateUserRequest) -> UpdateUserResponse:
     return uc.update_user_info(user_id, r.username, r.password, r.email)
 
-@app.put("/users_role/{user_id}", status_code=200, response_model=None)
+@app.put("/users_role/{user_id}")
 async def update_user_role(user_id: str, r: UpdateUserRoleRequest) -> UpdateUserRoleResponse:
     return uc.update_user_role(user_id, r.role)
 
-@app.post("/sessions", status_code=200, response_model=None)
+@app.post("/sessions")
 async def user_login(r: UserLoginRequest) -> UserLoginResponse:
     return sc.user_login(r.username, r.password)
 
-@app.get("/sessions_all",  status_code=200, response_model=None)
+@app.get("/sessions_all")
 async def get_all_sessions() -> list[GetSessionResponse]:
     return sc.get_all_sessions()
 
-@app.get("/sessions/{session_id}",  status_code=200, response_model=None)
+@app.get("/sessions/{session_id}")
 async def get_session(session_id: str) -> GetSessionResponse:
     return sc.get_session(session_id)
 
-@app.delete("/sessions/{session_id}",  status_code=200, response_model=None)
+@app.delete("/sessions/{session_id}")
 async def user_logout(session_id: str) -> UserLogoutResponse:
     return sc.user_logout(session_id)
