abb423adfb1a3327ed07f5f03d9ed25f41f628c4
EvitanRelta <shauntanzongzhi@gmail.com>
2023-10-30 10:55:08 +0800

    Protect users & questions services

--- backend_services/users_service/api/app/main.py
+++ backend_services/users_service/api/app/main.py
@@ -21,6 +21,9 @@ from shared_definitions.auth.core import TokenData
 from shared_definitions.auth.fastapi_dependencies import (
     decode_access_token_data,
     decode_refresh_token_data,
+    require_logged_in,
+    require_maintainer_role,
+    require_same_user_or_maintainer_role,
 )
 
 # create app
@@ -42,39 +45,39 @@ async def create_user(r: CreateUserRequest) -> CreateUserResponse:
     return uc.create_user(user_id, r.username, r.email, r.password)
 
 
-@app.get("/user_me")
+@app.get("/user_me", dependencies=[Depends(require_logged_in)])
 async def get_current_user(
     access_token_data: TokenData = Depends(decode_access_token_data),
 ) -> GetUserResponse:
     return uc.get_current_user(access_token_data)
 
 
-@app.get("/users/{user_id}")
+@app.get("/users/{user_id}", dependencies=[Depends(require_logged_in)])
 async def get_user(user_id: str) -> GetUserResponse:
     return uc.get_user(user_id)
 
 
-@app.get("/users_all")
+@app.get("/users_all", dependencies=[Depends(require_maintainer_role)])
 async def get_all_users() -> list[GetUserResponse]:
     return uc.get_all_users()
 
 
-@app.delete("/users_all")
+@app.delete("/users_all", dependencies=[Depends(require_maintainer_role)])
 async def delete_all_users() -> DeleteUserResponse:
     return uc.delete_all_users()
 
 
-@app.delete("/users/{user_id}")
+@app.delete("/users/{user_id}", dependencies=[Depends(require_same_user_or_maintainer_role)])
 async def delete_user(user_id: str) -> DeleteUserResponse:
     return uc.delete_user(user_id)
 
 
-@app.put("/users/{user_id}")
+@app.put("/users/{user_id}", dependencies=[Depends(require_same_user_or_maintainer_role)])
 async def update_user_info(user_id: str, r: UpdateUserRequest) -> UpdateUserResponse:
     return uc.update_user_info(user_id, r.username, r.password, r.email)
 
 
-@app.put("/users_role/{user_id}")
+@app.put("/users_role/{user_id}", dependencies=[Depends(require_maintainer_role)])
 async def update_user_role(user_id: str, r: UpdateUserRoleRequest) -> UpdateUserRoleResponse:
     return uc.update_user_role(user_id, r.role)
 
@@ -89,7 +92,7 @@ async def user_logout(refresh_token: str | None = Cookie(None)) -> Response:
     return sc.user_logout(refresh_token)
 
 
-@app.get("/refresh")
+@app.get("/refresh", dependencies=[Depends(require_logged_in)])
 async def refresh_access_token(
     refresh_token_data: TokenData = Depends(decode_refresh_token_data),
 ) -> Response:
