8c15c5c309bd7923750e2a30b2c54423afd92677
Yaladah <jeromeseet@hotmail.com>
2023-11-01 17:41:55 +0800

    Display random question when entering collab room.

--- frontend/src/components/CollaborationRoom/QuestionDescription.tsx
+++ frontend/src/components/CollaborationRoom/QuestionDescription.tsx
@@ -1,15 +1,32 @@
 import React from 'react'
+import { useAllQuestions } from '../../stores/questionStore.ts'
 
 const QuestionDescription: React.FC = () => {
+    const { data: questions } = useAllQuestions()
+    const easyQuestions = questions.filter((question) => question.complexity === 'Easy')
+    const randomQuestion = easyQuestions[Math.floor(Math.random() * easyQuestions.length)]
+
     return (
         <div style={{ padding: '10px 20px' }}>
             <h2 style={{ margin: '0', fontWeight: 'normal', fontSize: '1.5rem' }}>
-                Question Title
+                {randomQuestion.title}
             </h2>
-            <h2 className={`complexity-color-Easy`} style={{ margin: '0', fontSize: '1.25rem' }}>
-                Easy
+            <h2
+                className={`complexity-color-${randomQuestion.complexity}`}
+                style={{ margin: '0', fontSize: '1.25rem' }}
+            >
+                {randomQuestion.complexity}
             </h2>
-            <div style={{ marginTop: '1.5rem' }}>Question description</div>
+            <pre
+                id='alert-dialog-description'
+                style={{
+                    fontFamily: 'courier',
+                    color: 'white',
+                    whiteSpace: 'pre',
+                }}
+            >
+                {randomQuestion.description}
+            </pre>
         </div>
     )
 }
