f8176d1fac0b1d01f93127744674c6bc013fb20f
EvitanRelta <shauntanzongzhi@gmail.com>
2023-11-03 19:16:42 +0800

    Refactor backend login code

--- backend_services/users_service/api/app/controllers/sessions_controller.py
+++ backend_services/users_service/api/app/controllers/sessions_controller.py
@@ -12,29 +12,28 @@ from utils import sessions_util, users_util
 
 
 def user_login(username: str, password: str) -> JSONResponse:
+    if not users_util.username_exists(username):
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Account does not exist",
+        )
+
     hashed_password = hashlib.md5(password.encode()).hexdigest()
 
-    login_result = sessions_util.is_valid_login(
-        username, hashed_password
-    )  # returns False if invalid login
+    user_id, role = sessions_util.get_user_id_and_role(username, hashed_password)
 
-    if login_result:
-        user_id, role = login_result
-        access_token = create_access_token(user_id, role)
-        refresh_token = create_refresh_token(user_id, role)
-        sessions_util.store_refresh_token(refresh_token)
+    if user_id is None or role is None:
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Invalid password")
 
-        response_payload = UserLoginResponse(message=f"User {username} successfully logged in")
-        response = JSONResponse(content=response_payload.model_dump(mode="json"))
-        response.set_cookie(key="refresh_token", value=refresh_token)
-        response.set_cookie(key="access_token", value=access_token)
-        return response
-    else:
-        if users_util.username_exists(username):
-            raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Invalid password")
-        raise HTTPException(
-            status_code=status.HTTP_401_UNAUTHORIZED, detail="Account does not exist"
-        )
+    access_token = create_access_token(user_id, role)
+    refresh_token = create_refresh_token(user_id, role)
+    sessions_util.store_refresh_token(refresh_token)
+
+    response_payload = UserLoginResponse(message=f"User {username} successfully logged in")
+    response = JSONResponse(content=response_payload.model_dump(mode="json"))
+    response.set_cookie(key="refresh_token", value=refresh_token)
+    response.set_cookie(key="access_token", value=access_token)
+    return response
 
 
 def user_logout(refresh_token: str | None) -> Response:
