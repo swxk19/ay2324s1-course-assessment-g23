8b44903370df96b1dfe370abbcca7c1545e7a10c
swxk19 <s.wxk19@gmail.com>
2023-11-08 15:52:17 +0800

    Format frontend code

--- frontend/src/pages/Room.tsx
+++ frontend/src/pages/Room.tsx
@@ -7,7 +7,6 @@ import ConfirmationBox from '../components/CollaborationRoom/ConfirmationBox.tsx
 import QuestionDescription from '../components/CollaborationRoom/QuestionDescription.tsx'
 import RoomNavBar from '../components/CollaborationRoom/RoomNavBar.tsx'
 import '../styles/Room.css'
-import VideoChat from '../components/VideoChat.tsx'
 
 export const Room = () => {
     const [showConfirmation, setShowConfirmation] = useState(false)
