b39c2010eb384c9495dc6b345ab939741834addb
Yaladah <jeromeseet@hotmail.com>
2023-11-10 23:00:21 +0800

    Implement deletion of room if empty.

--- backend_services/communication_service/app/data_classes.py
+++ backend_services/communication_service/app/data_classes.py
@@ -34,6 +34,21 @@ class Room:
 
     def is_full(self) -> bool:
         return len(self.clients) > 2
+    
+    def is_empty(self) -> bool:
+        return not bool(self.clients)
+    
+    def remove_user(self, user_id: str) -> None:
+        if user_id in self.clients:
+            del self.clients[user_id]
+            
+    def add_user(self, user: UserWebSocket) -> None:
+        self.clients[user.user_id] = user
+        
+    def delete_if_empty(self, rooms_dict: dict[str, 'Room'], room_id: str) -> None:
+        if self.is_empty():
+            # Optionally, perform any cleanup or additional actions before deleting
+            del rooms_dict[room_id]
 
 
 @dataclass
